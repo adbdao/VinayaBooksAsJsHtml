ff99b792bf44dbc2851d0bea29e586277f0def3c
magga <adb.dao@gmail.com>
2018-12-24 18:15:15 +0800

    del file in DirToIndex,update:nanputo history and index

--- dirToIndex.js
+++ dirToIndex.js
@@ -1,7 +1,8 @@
 // 導入模組
 var fs = require('fs')
 var path = require('path')
-
+// 配合timeEnd()成對出現。 打印出代碼執行的時間
+console.time('共耗費了')
 // 規範化檔案路徑
 var h = path.normalize('./')
 // 取得當前目錄下所有檔案及資料夾
@@ -23,6 +24,8 @@ for (var i = 0; i < k.length; i++) {
     if (/var ArrLi\s?\=/.test(k[i])) {
         k[i] = 'var ArrLi = ["' + d.join('","') + '"]'
     }
+    // 刪除不需要的檔
+    k[i]=k[i].replace(/\,"[^\.]+\.xlsx"/g,'').replace(',"index.html"','').replace(',"dirToIndex.js"','').replace(',"x-to-h"','')
 }
 // 好像無法在for of中使用正規，以下失敗
 // for(var i of k) {
@@ -56,3 +59,6 @@ console.log('index.html is OK')
 // fs.writeFileSync(path.normalize('./DirName.txt'), '["' + d.join('","') + '"]', 'utf8')
 // // 完成時返回通知
 // console.log('DirName is OK')
+
+// 'test'名字要和time()中的名字保持一致
+console.timeEnd('共耗費了')
