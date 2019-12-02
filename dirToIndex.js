964c382c39b6ff7d8c1b503a297d565edce46329
magga <adb.dao@gmail.com>
2019-12-02 12:48:45 +0800

    add new Index of white for moble

--- dirToIndex.js
+++ dirToIndex.js
@@ -25,40 +25,13 @@ for (var i = 0; i < k.length; i++) {
         k[i] = 'var ArrLi = ["' + d.join('","') + '"]'
     }
     // 刪除不需要的檔
-    k[i]=k[i].replace(/\,"[^\.]+\.xlsx"/g,'').replace(',"index.html"','').replace(',"dirToIndex.js"','').replace(',"x-to-h"','')
+    k[i]=k[i].replace(',"index.html"','').replace(',"x-to-h"','')
 }
-// 好像無法在for of中使用正規，以下失敗
-// for(var i of k) {
-//     if (/var ArrLi\s?\=/.test(k[i])) {
-//     if (i.search('var ArrLi=')) {
-//         i = 'var ArrLi = ["' + d.join('","') + '"]'
-//     }
-// }
 
 // 用相對路徑寫入檔案
 fs.writeFileSync('index.html', k.join('\n'), 'utf8')
 // 完成時返回通知
 console.log('index.html is OK')
-
-// // 讀取Lum1.html
-// var Lum1 = fs.readFileSync('Lum1.html', 'utf8')
-// // 在陣列中找出「檔名陣列」
-// var L = Lum1.split('\n')
-// for (var i = 0; i < L.length; i++) {
-//     if (/var ArrLi\s?\=/.test(L[i])) {
-//         L[i] = 'var ArrLi = ["' + d.join('","') + '"]'
-//     }
-// }
-// // 用相對路徑寫入檔案
-// fs.writeFileSync('Lum1.html', L.join('\n'), 'utf8')
-// // 完成時返回通知
-// console.log('Lum1.html is OK')
-
-// 本來只寫入另一檔案DirName.txt，還要人工複製，後來可以直接寫入index.html
-// 用相對路徑寫入檔案
-// fs.writeFileSync(path.normalize('./DirName.txt'), '["' + d.join('","') + '"]', 'utf8')
-// // 完成時返回通知
-// console.log('DirName is OK')
 
 // 'test'名字要和time()中的名字保持一致
 console.timeEnd('共耗費了')
