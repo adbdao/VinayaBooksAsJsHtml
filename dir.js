53fc2905e7c9017bdc0cb5056e70ff4aff0b80ba
magga <adb.dao@gmail.com>
2020-12-31 16:32:02 +0800

    add D3.js tree only && Del readme CC.

--- dir.js
+++ dir.js
@@ -24,7 +24,7 @@ for (var i = 0; i < k.length; i++) {
     if (/var ArrLi\s?\=/.test(k[i])) {
         k[i] = 'var ArrLi = ["' + d.join('","') + '"]'
         // 刪除不需要的檔
-        k[i] = k[i].replace(',".git"', '').replace(',"index.html"', '').replace(',"dirToIndex.js"', '').replace(',"dir.js"', '').replace(',"x-to-h"', '').replace(',"debug.log"', '').replace(',"_config.yml"', '')
+        k[i] = k[i].replace(',".git"', '').replace(',"index.html"', '').replace(',"dirToIndex.js"', '').replace(',"dir.js"', '').replace(',"x-to-h"', '').replace(',"debug.log"', '').replace(',"_config.yml"', '').replace(',".gitignore"', '')
     }
 }
 
