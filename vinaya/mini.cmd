758219980ed09e158c5b3f4c395324398c0e1b8d
DaoChung <adb.dao@gmail.com>
2025-04-01 17:37:49 +0800

    upload JY && .adb.ini

--- vinaya/mini.cmd
+++ vinaya/mini.cmd
@@ -2,9 +2,13 @@ cd /d D:\F\Accelon3\佛學資料庫(第三版，圖書館與教育單位用，不可流通)
 
 copy 戒律書畾.adb.ini D:\GitHub\VinayaBooksAsJsHtml\vinaya
 copy taisho.adb.ini D:\GitHub\VinayaBooksAsJsHtml\vinaya
+copy rdg2011.adb.ini D:\GitHub\VinayaBooksAsJsHtml\vinaya
+copy zhimin.adb.ini D:\GitHub\VinayaBooksAsJsHtml\vinaya
 
 copy 戒律書畾.adb.ini D:\GitHub\my-adb.ini
 copy taisho.adb.ini D:\GitHub\my-adb.ini
+copy rdg2011.adb.ini D:\GitHub\my-adb.ini
+copy zhimin.adb.ini D:\GitHub\my-adb.ini
 
 cd /d D:\GitHub\my-adb.ini
 node dir.js
@@ -17,5 +21,8 @@ cd /d D:\GitHub\VinayaBooksAsJsHtml\
 node dir.js
 
 git add .
-git commit -am "add boddhisata sila && upload JY && .adb.ini"
+git commit -am "upload JY && .adb.ini"
 git push
+git push
+git push --tags
+git push --tags
